4058adcc9ba0fcee3de929189c7d037bca99656b
root <francescopagano1999@outlook.it>
2021-10-26 11:10:42 +0000

    Uploading some other last improvements

--- assignment.py
+++ assignment.py
@@ -1,11 +1,11 @@
-"""-------------IMPORT LIBRARIES-------------"""
+#-------------IMPORT LIBRARIES----------------------
 from __future__ import print_function
 import time
 
-"""-------------IMPORT CLASS ROBOT-------------"""
+#-------------IMPORT CLASS ROBOT--------------------
 from sr.robot import *
 
-"""-------------DEFINING GOLBAL VARIABLES-------------"""
+#-------------DEFINING GOLBAL VARIABLES-------------
 
 """ float: Threshold for the control of the linear distance"""
 a_th = 2.3
@@ -18,7 +18,7 @@ gold_th=1
 """ float: Threshold for the activation of the grab routine"""
 silver_th=1.5
 
-"""-------------DEFINING FUNCTIONS-------------"""
+#-------------DEFINING FUNCTIONS-------------
 
 def drive(speed, seconds):
     """
@@ -32,6 +32,8 @@ def drive(speed, seconds):
     time.sleep(seconds)
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
+    
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def turn(speed, seconds):
     """
@@ -45,6 +47,8 @@ def turn(speed, seconds):
     time.sleep(seconds)
     R.motors[0].m0.power = 0
     R.motors[0].m1.power = 0
+    
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def find_silver_token():
     """
@@ -68,6 +72,8 @@ def find_silver_token():
 	return -1, -1
     else:
    	return dist, rot_y
+   	
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def find_golden_token():
     """
@@ -89,6 +95,8 @@ def find_golden_token():
     else:
    	return dist
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 def find_golden_token_left():
     """
     Function to compute the closest golden token distace on the left of the robot.
@@ -105,6 +113,8 @@ def find_golden_token_left():
 	return -1
     else:
    	return dist
+   	
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    	
 def find_golden_token_right():
     """
@@ -123,6 +133,8 @@ def find_golden_token_right():
 	return -1
     else:
    	return dist
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    	
 def grab_routine(rot_silver, dist_silver): 
     """
@@ -136,11 +148,11 @@ def grab_routine(rot_silver, dist_silver):
     	print("Found it!")
     	if R.grab():
 	    print("Grabbed!")
-	    turn(24, 3)
+	    turn(20, 3)
 	    drive(15, 0.8)
 	    R.release()
 	    drive(-15,0.8)
-	    turn(-24,3)
+	    turn(-20,3)
     elif -a_th<=rot_silver<=a_th:
 	    drive(40, 0.5) 
 	    print("Correct angle")
@@ -151,27 +163,44 @@ def grab_routine(rot_silver, dist_silver):
 	    print("Right a bit...")
 	    turn(+5, 0.3)
 
-def turn_method(left_dist, right_dist):
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+def turn_method(left_dist, right_dist, dist_gold):
+	"""
+	Function that implements the turn decision method
+	Arguments: 
+		left_dist (float): distance of the closest gloden token on the left of the robot
+		right_dist (float): distance of the closest gloden token on the right of the robot
+	No returns.	
+	"""	
 	print("Where's the wall?")	
 	if left_dist>1.2*right_dist:
-		drive(-15,0.4)
-		turn(-20, 0.4)
-		print("The wall is on the right at a distance of: "+ str(right_dist))	
+		print("The wall is on the right at a distance of: "+ str(right_dist))
+		while dist_gold<gold_th:
+			dist_gold=find_golden_token()
+			turn(-10, 0.1)
+			print("I'm turning left")
 	elif right_dist>1.2*left_dist:
-		drive(-15,0.4)
-		turn(20, 0.4)
 		print("The wall is on the left at a distance of:  "+ str(left_dist))
+		while dist_gold<gold_th:
+			dist_gold=find_golden_token()
+			turn(10, 0.1)
+			print("I'm turning right")
 	else:
 		drive(15,0.5)
-		print("Left and right distances are similar, i'll go straight ahead")
+		print("Left and right distances are similar, i'll go straight")
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 def main():
 	while 1:  
 			#Updating variables value for every while cycle
+			
 			dist_silver, rot_silver = find_silver_token()
 			dist_gold=find_golden_token()
 			left_dist=find_golden_token_left()
 			right_dist=find_golden_token_right()
+			print("dist_gold= "+ str(dist_gold))
 			#Check if gold token are detected, if no gold token are detected go straight ahead.						
 			if (dist_gold>gold_th and dist_silver>silver_th) or (dist_gold>gold_th and dist_silver==-1):
 					print("I'll go straight ahead")
@@ -179,13 +208,14 @@ def main():
 			#If gold token are detected, then check where the wall is			
 			elif dist_gold<gold_th and dist_gold!=-1:
 			#The robot decides where to turn
-					turn_method(left_dist, right_dist)					
+					turn_method(left_dist, right_dist, dist_gold)					
 			if dist_silver<silver_th and dist_silver!=-1: 
 			#If any silver token closer than silver_th is detected, the grab routine will start
 		    		print("Silver is close")
 		    		grab_routine(rot_silver, dist_silver)	
-		    		    				
-"""-------------MAIN() CALL-------------"""
+		    			    		    				
+#-----------------main() CALL-------------------
+
 main()
     	
 
